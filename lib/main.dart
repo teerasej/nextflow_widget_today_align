7f2a6db75beae11a9a5f6d71d97755b0dd54d063
Teerasej <teerasej@gmail.com>
2020-08-02 20:45:52 +0700

    complete

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,45 @@ class _MyHomePageState extends State<MyHomePage> {
         appBar: AppBar(
           title: Text(widget.title),
         ),
-        body: Container());
+        body: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              child: Stack(
+                children: <Widget>[
+                  Image(
+                    height: 300,
+                    fit: BoxFit.cover,
+                    image: AssetImage('images/teach1.jpg'),
+                  ),
+                  Opacity(
+                    opacity: 0.8,
+                    child: Container(
+                      color: Colors.blue,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Align(
+                      alignment: Alignment(-1, 0.57),
+                      child: Text(
+                        'แนะนำภาควิชา',
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed odio quis dolor condimentum gravida. Aliquam interdum felis nibh, sit amet elementum leo feugiat at. Suspendisse ut sodales libero. Sed viverra mollis egestas. Suspendisse dapibus velit urna, id mollis diam luctus sollicitudin. Fusce eget interdum ipsum, et condimentum leo. Praesent nec est eu ipsum tristique scelerisque. Nullam viverra bibendum ipsum, nec scelerisque erat pellentesque vitae.'),
+            )
+          ],
+        ));
   }
 }
