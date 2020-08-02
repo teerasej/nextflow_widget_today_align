9d4d400c811df4706a61626232ffe5fe482347f6
Teerasej <teerasej@gmail.com>
2020-08-02 22:22:21 +0700

    for start

--- lib/main.dart
+++ lib/main.dart
@@ -53,14 +53,11 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                   Padding(
                     padding: EdgeInsets.all(20),
-                    child: Align(
-                      alignment: Alignment(-1, 0.57),
-                      child: Text(
-                        'แนะนำภาควิชา',
-                        style: TextStyle(
-                          fontSize: 30,
-                          color: Colors.white,
-                        ),
+                    child: Text(
+                      'แนะนำภาควิชา',
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.white,
                       ),
                     ),
                   )
